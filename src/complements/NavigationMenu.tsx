c030d18d54eb98e108924130a328af3690de976c
jaime-neto <jaime.neto99@hotmail.com>
2023-04-03 22:58:35 -0300

    Adicionando link ao botão do github.

--- src/complements/NavigationMenu.tsx
+++ src/complements/NavigationMenu.tsx
@@ -3,12 +3,18 @@ import { Link } from "react-router-dom";
 
 const NavigationMenu = ()=>{
 
+    var url = "https://github.com/jaime-neto";
+
+    function openGitHub(){
+        window.open(url, '_blank');
+    }
+
     return(
         <div>
             <div className="submenu">
                 <Link to="/"><input type="button" id="home" value="Home"/></Link>
                 <span className="espaco"></span>
-                <Link to="/github"><input type="button" id="github" value="GitHub"/></Link>
+                <input onClick={openGitHub} type="button" id="github" value="GitHub"/>
                 <span className="espaco"></span>
                 <Link to="/about"><input type="button" id="about" value="About"/></Link>
                 <span className="espaco"></span>
