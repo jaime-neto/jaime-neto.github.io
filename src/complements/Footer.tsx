3286b598fa12bbe576342a6dd0472846a628e78c
jaime-neto <jaime.neto99@hotmail.com>
2023-04-08 23:02:32 -0300

    modificando os icones do footer e linkando eles

--- src/complements/Footer.tsx
+++ src/complements/Footer.tsx
@@ -1,6 +1,23 @@
 import React from "react";
+import {ImFacebook} from "react-icons/im";
+import {AiFillGithub} from "react-icons/ai";
+import {BsInstagram} from "react-icons/bs";
 
 const Footer = ()=>{
+
+    var urlGit = "https://github.com/jaime-neto";
+    var urlFacebook = "https://www.facebook.com/jaime.neto.75";
+    var urlInstagram = "https://www.instagram.com/jaimeneto7/";
+
+    function abrirURL(redeSocial: string){
+        if(redeSocial === "facebook"){
+            window.open(urlFacebook, '_blank');
+        }else if(redeSocial === "git"){  
+            window.open(urlGit, '_blank');
+        }else if(redeSocial === "instagram"){
+            window.open(urlInstagram, '_blank');
+        }
+    }
 
     return(
         <div>
@@ -11,9 +28,22 @@ const Footer = ()=>{
             </div>
 
             <div className="redes">
-                <p>inst</p>
-                <p>fac</p>
-                <p>git</p>
+                <button onClick={event => abrirURL("facebook")} id="face" type="button">
+                    <ImFacebook/>
+                </button>
+                
+                <p></p>
+                
+                <button onClick={event => abrirURL("git")} id="git" type="button">
+                    <AiFillGithub/>
+                </button>
+        
+                <p></p>
+
+                <button onClick={event => abrirURL("instagram")} id="inst" type="button">
+                    <BsInstagram/>
+                </button>
+
             </div>
         </div>
     );
