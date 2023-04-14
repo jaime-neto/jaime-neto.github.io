83e39213c76ff9be3de07337b3e9c4a061827a49
jaime-neto <jaime.neto99@hotmail.com>
2023-04-13 22:34:54 -0300

    mudando a cor do hover do menu e alterando o footer

--- src/complements/Footer.tsx
+++ src/complements/Footer.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import {ImFacebook} from "react-icons/im";
 import {AiFillGithub} from "react-icons/ai";
 import {BsInstagram} from "react-icons/bs";
+import {FiLinkedin} from "react-icons/fi";
 
 const Footer = ()=>{
 
-    var urlGit = "https://github.com/jaime-neto";
-    var urlFacebook = "https://www.facebook.com/jaime.neto.75";
-    var urlInstagram = "https://www.instagram.com/jaimeneto7/";
+    const urlGit = "https://github.com/jaime-neto";
+    const urlFacebook = "https://www.facebook.com/jaime.neto.75";
+    const urlInstagram = "https://www.instagram.com/jaimeneto7/";
+    const urlLinkedin = "https://www.linkedin.com/in/jaime-neto-19a2a620b";
 
     function abrirURL(redeSocial: string){
         if(redeSocial === "facebook"){
@@ -16,6 +18,8 @@ const Footer = ()=>{
             window.open(urlGit, '_blank');
         }else if(redeSocial === "instagram"){
             window.open(urlInstagram, '_blank');
+        }else if(redeSocial ==="linkedin"){
+            window.open(urlLinkedin, '_blank');
         }
     }
 
@@ -43,6 +47,14 @@ const Footer = ()=>{
                 <button onClick={event => abrirURL("instagram")} className="item-footer" type="button">
                     <BsInstagram/>
                 </button>
+
+                <p></p>
+
+                <button onClick={event => abrirURL("linkedin")} className="item-footer" type="button">
+                    <FiLinkedin/>
+                </button>
+                
+                <div id="riscoFooter"></div>
 
             </div>
         </div>
