f34de1e70af69fb7859645225996b07853fe04a9
jaime-neto <jaime.neto99@hotmail.com>
2023-05-30 23:10:53 -0300

    mudança no texto da tela about

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -14,24 +14,23 @@ const About = ()=>{
                 </Typography>
 
                 <Typography variant="body1" align="justify">
-                    Olá, meu nome é Jaime e sou formado em Bacharelado em Tecnologia da Informação. Moro em Serrinha dos Pintos-RN e tenho habilidade em 
-                    encontrar soluções para diversos tipos de problemas, especialmente quando envolvem programação. Embora tenha começado na área por acaso,
-                    durante o curso de TI, fui me interessando cada vez mais e me tornando familiarizado com as tecnologias e ferramentas utilizadas na área.
-                    Estou sempre em busca de aprendizado e aprimoramento para me tornar um profissional competente e atualizado.
+                    Olá, meu nome é Jaime, sou formado em Tecnologia da Informação (TI) e estou em busca de uma oportunidade como desenvolvedor de software. 
+                    Ao longo da minha jornada acadêmica, obtive um amplo conjunto de habilidades em diversas áreas tecnológicas, tais como fundamentos da 
+                    programação, desenvolvimento de software, banco de dados, redes de computadores e sistemas operacionais. Além disso, pude aprimorar minha 
+                    capacidade de lidar com problemas complexos de forma eficiente e eficaz. Estou entusiasmado para aplicar esses conhecimentos e competências
+                    em um ambiente profissional e contribuir para projetos desafiadores e inovadores. 
                     <Typography></Typography>
-                    Ingressei no curso de bacharelado em Tecnologia da Informação em 2016. Durante toda a minha jornada acadêmica, adquiri conhecimentos em 
-                    diversas tecnologias e aprimorei minha habilidade em lidar com problemas do cotidiano. A visão proporcionada pelo curso ampliou minha 
-                    perspectiva de mundo e me preparou para atuar no mercado de trabalho. A grade curricular tinha ênfase em Engenharia de Software, o que me 
-                    levou a cursar disciplinas como metodologias ágeis, análise de requisitos, qualidade e teste de software, consolidando minha formação nesta 
-                    área.
+                    Trabalho como Técnico de Telecomunicações desde 2016, desempenhando atividades como instalação e manutenção de equipamentos de rádio e fibra óptica.
+                    Ao longo da minha trajetória profissional, desenvolvi habilidades em comunicação interpessoal, liderança de equipes e gestão de situações sob pressão. 
                     <Typography></Typography>
-                    Trabalho como Técnico de Telecomunicações desde 2016, desempenhando atividades como instalação e manutenção de equipamentos de rádio e 
-                    fibra óptica. Ao longo da minha trajetória profissional, desenvolvi habilidades em comunicação interpessoal, liderança de equipes e gestão
-                    de situações sob pressão.
-                  
-                    No momento, busco uma oportunidade para atuar como desenvolvedor de software, área na qual sou formado. A seguir, compartilho algumas das 
-                    tecnologias com as quais tenho experiência e que tenho utilizado em projetos pessoais recentes:
-                    
+                    Hobbies e Interesses: Além da minha paixão por TI, tenho também alguns hobbies que me ajudam a manter uma vida equilibrada. Sou entusiasta da corrida
+                    de rua, através dessa prática, desenvolvo disciplina, perseverança e uma mentalidade de melhoria contínua. Além disso, sou um adepto fervoroso da 
+                    musculação, valorizando os benefícios que essa atividade traz para a saúde física e mental. Através da musculação, construo força, resistência e 
+                    bem-estar geral. Essas atividades complementares não apenas me ajudam a manter uma vida ativa e saudável, mas também aprimoram minha habilidade em 
+                    lidar com desafios e metas.
+                    <Typography></Typography>
+                    Algumas das tecnologias com as quais tenho experiência e que tenho utilizado em projetos pessoais recentes:
+
                     <pre>&gt; C/C++        &gt; JavaScript</pre>
                     <pre>&gt; Java         &gt; PostgreSQL</pre>
                     <pre>&gt; Git          &gt; Node.js</pre>
